245e4bbb580d06223bba88092ace7dbea26b470c
brunogsiq <brunogsiq@gmail.com>
2025-03-15 15:03:16 -0300

    Revisar projeto - 1 - revisar yaml quando constar 2 - revisar testes 3 - revisar informações 4 - revisar git ignore

--- YouShop/FrontEnd/_ArtefatosDeTestes/_Dooumentacao/02 - Casos de Testes Automatizados.js
+++
@@ -1,7 +0,0 @@
-/*
-    Dado que a tela principal da aplicação foi acessada,
-    Quando os elementos são apresentados,
-    Então o sistema deverá exibir o logotipo da empresa para comprovar o acesso correto
-
-    
-*/
